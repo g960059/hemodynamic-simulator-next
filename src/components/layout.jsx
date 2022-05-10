f8a4c47f6e410dc1de74049ce6aa47086c8e6bcc
g960059 <g960059@gmail.com>
2022-05-10 15:16:02 +0900

    fix login

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -13,6 +13,7 @@ import {nanoid} from '../utils/utils'
 
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 
@@ -91,6 +92,7 @@ function Layout(props) {
   const router = useRouter()
   const {data:user} = useObservable(`user_${auth?.currentUser?.uid}`,user$)
   const [dialogOpen, setDialogOpen] = useState(false);
+  const [signInWithGoogle, _, loading, error] = useSignInWithGoogle(auth);
 
   const [anchorEl, setAnchorEl] = useState(null);
   const [newItemAnchorEl, setNewItemAnchorEl] = useState(null);
@@ -149,23 +151,10 @@ function Layout(props) {
               <DialogContentText variant="body2">
                 循環動態シミュレーターで様々な病態や治療法への理解を深めていきましょう。
               </DialogContentText>
-              <Box width={1} display='flex' justifyContent='center' alignItems='center' 
-                sx={{"& .firebaseui-idp-button":{
-                  transition: "background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-                  boxShadow: "rgb(0 0 0 / 10%) 0px 2px 4px -2px",
-                  backgroundColor: "#EFF6FB99",
-                  border: "1px solid rgba(92, 147, 187, 0.17)",
-                  "&:hover":{
-                      backgroundColor: "rgba(239, 246, 251, 0.6)",
-                      borderColor: "rgb(207, 220, 230)"
-                  },
-                  "& .firebaseui-idp-text":{
-                    fontSize: "1rem",
-                    color:"black"
-                  }
-                }}}>
-                <StyledAuth/>
-              </Box>
+                <button variant='contained' onClick={()=>{signInWithGoogle()}} className="btn-neumorphic mx-auto text-base cursor-pointer flex items-center justify-center my-4 text-black py-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48" class="abcRioButtonSvg mr-3"><path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/><path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"/><path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"/><path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"/><path fill="none" d="M0 0h48v48H0z"/></svg>
+                  Sign in with Google
+                </button>
               <DialogContentText sx={{mt:.5}} variant="body2">
                 利用規約、プライバシーポリシーに同意したうえでログインしてください。
               </DialogContentText>
