76ccfe0423e9f97143965493067a5f16577b654e
g960059 <g960059@gmail.com>
2022-08-05 20:59:40 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -25,12 +25,8 @@ module.exports = {
          // Important: return the modified config
         return config
     },
-    i18n: {
-        locales: ['en', 'ja'],
-        defaultLocale: 'ja'
-    },
     images: {
         domains: ['lh3.googleusercontent.com',"firebasestorage.googleapis.com", "127.0.0.1", "localhost"],
     },
-    experimental: { esmExternals: false }
+    experimental: { esmExternals: true },
   }
