85bf7264e25ef964db8dc89e3edd17fb793ff763
g960059 <g960059@gmail.com>
2023-08-31 17:23:56 +0900

    fix ogp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import { CacheProvider } from '@emotion/react';
 import {CssBaseline} from '@mui/material';
 import GoogleAnalytics from '../src/components/GoogleAnalytics'
 import {useTranslation} from '../src/hooks/useTranslation'
-import { DefaultSeo } from 'next-seo';
+// import { DefaultSeo } from 'next-seo';
 import "../src/styles/globals.css"
 import { NextPage } from 'next';
 import createEmotionCache from '../src/utils/createEmotionCache'
@@ -50,20 +50,20 @@ export default function MyApp({Component, pageProps }: AppPropsWithLayout) {
       <CacheProvider value={cache}>
         <Head>
           <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <title>CircleHeart</title>
+          <meta name="description" content={t["Description"]} />
+          <meta property="og:title" content="CircleHeart" />
+          <meta property="og:description" content={t["Description"]} />
+          <meta property="og:image" content="https://www.circleheart.dev/ogp.png" />
+          <meta property="og:url" content="https://www.circleheart.dev/" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@CircleHeart_" />
+          <meta name="twitter:creator" content="@CircleHeart_" />
+          <meta name="twitter:title" content="CircleHeart" />
+          <meta name="twitter:description" content={t["Description"]} />
+          <meta name="twitter:image" content="https://www.circleheart.dev/ogp.png" />
         </Head>
-        <DefaultSeo
-          defaultTitle="CircleHeart"
-          title={"CircleHeart"+" | "+t["Description"]}
-          titleTemplate="CircleHeart"
-          canonical="https://www.circleheart.dev/"
-          description={t["Description"]}
-          openGraph={{
-            type: 'website',
-            title: "CircleHeart", 
-            site_name: "CircleHeart",
-            url: 'https://www.circleheart.dev/',
-          }}
-        />
+
         <GoogleAnalytics />
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
